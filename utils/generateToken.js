6806f61322b6c27e4b6a3a58c60a8a73cb492938
tom <46255836+Nyumat@users.noreply.github.com>
2022-10-28 01:40:41 -0700

    [TRCKME-10] Added Register User Functionality + UI (#1)

---
+++ utils/generateToken.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const generateAuthToken = (user) => {
+  const jwtSecretKey = process.env.JWT_SECRET_KEY;
+  const token = jwt.sign(
+    {
+      _id: user._id,
+      username: user.username,
+    },
+    jwtSecretKey
+  );
+
+  return token;
+};
+
+export default generateAuthToken;
