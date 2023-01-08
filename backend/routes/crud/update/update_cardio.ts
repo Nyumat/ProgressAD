765494a34d1029e5b58d64a2b2e6571b99f2884a
Thomas <nyumat18@gmail.com>
2023-01-07 20:47:27 -0800

    [Update] Finished CRUD Route(s) Migration (#11)

--- backend/routes/crud/update/update_cardio.ts
+++ backend/routes/crud/update/update_cardio.ts
@@ -1,8 +1,9 @@
-import { User } from "../../../models/user.js";
-import { Router } from "express";
+//@ts-nocheck
+import User from "../../../models/user.js";
+import { Router, Request, Response }  from "express";
 const router = Router();
 
-router.put("/", async (req, res) => {
+router.put("/", async (req: Request, res: Response) => {
 	try {
 		const { username, distance, timeSpent, machine_id } = req.body;
 
@@ -15,7 +16,7 @@ router.put("/", async (req, res) => {
 		let workout = user.workouts[user.workouts.length - 1];
 
 		let machine = workout.machines.find(
-			(machine) => machine.machine_id === machine_id
+			(machine: { machine_id: any; }) => machine.machine_id === machine_id
 		);
 
 		if (!machine) {
@@ -27,7 +28,7 @@ router.put("/", async (req, res) => {
 
 		await user.save();
 
-		res.status(200).json({ msg: "Cardio added successfully.", workout });
+		res.status(200).json({ msg: "Cardio data added successfully.", workout });
 	} catch (error) {
 		res.send(error);
 	}
--- backend/routes/crud/update/update_set.ts
+++ backend/routes/crud/update/update_set.ts
@@ -1,8 +1,9 @@
-import { User } from "../../../models/user.js";
-import { Router } from "express";
+//@ts-nocheck
+import User from "../../../models/user.js";
+import { Router, Request, Response }  from "express";
 const router = Router();
 
-router.post("/", async (req, res) => {
+router.post("/", async (req: Request, res: Response) => {
 	try {
 		const { username, machine_id, sets } = req.body;
 
@@ -14,15 +15,19 @@ router.post("/", async (req, res) => {
 
 		let workout = user.workouts[user.workouts.length - 1];
 
+		if (!workout) {
+			return res.status(400).json({ msg: "You have no current workout." });
+		}
+
 		let machine = workout.machines.find(
-			(machine) => machine.machine_id === machine_id
+			(machine: { machine_id: any; }) => machine.machine_id === machine_id
 		);
 
 		if (!machine) {
 			return res.status(400).json({ msg: "Machine does not exist." });
 		}
 
-		sets.forEach((set) => {
+		sets.forEach((set: any) => {
 			machine.sets.push(set);
 		});
 
