1fab3f47e813db50f5cc2521e0eedd46059b8c48
Thomas <nyumat18@gmail.com>
2023-01-07 20:51:03 -0800

    Middleware Converted (#11)

--- backend/utils/setHeaders.ts
+++ backend/utils/setHeaders.ts
@@ -1,12 +1,10 @@
-import { Router } from "express";
+import { Router, Request, Response }  from "express";
 const router = Router();
 
-router.post("/", async (req, res) => {
+router.post("/", async (req: Request, res: Response) => {
 	res.header("Access-Control-Allow-Origin", "*");
-	res.header(
-		"Access-Control-Allow-Headers",
-		"Origin, X-Requested-With, Content-Type, Accept"
-	);
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
 });
 
 export default router;
